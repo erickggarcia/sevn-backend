8d20f26e31ee5781c7903475b6f156349f2e4c2c
Erick Garcia <erickgegarcia@gmail.com>
2024-05-25 14:19:30 -0300

    [update] atualizando rota principal para articles/mainArticle, responsável por retornar apenas os 3 principais artigos

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,9 +3,10 @@ import { Database } from '../controllers/controllers.js'
 
 const route = express.Router()
 
-route.get('/articles', (req, res) => {
+route.get('/articles/mainArticles', (_, res) => {
     const data = Database.selectAll('articles')
-    return res.writeHead(201).end(JSON.stringify(data))
+    const mainArticles = data.slice(0, 3)
+    return res.writeHead(201).end(JSON.stringify(mainArticles))
 })
 
 export default route
