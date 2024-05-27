15a978b99e66bcd21fb3a83d44f7b3f3253f2666
erickggarcia <erickgegarcia@gmail.com>
2024-05-27 10:31:52 -0300

    [fix] corrigindo uso do cors na aplicação

--- src/server.js
+++ src/server.js
@@ -4,9 +4,9 @@ import route from './routes/routes.js'
 
 const app = express()
 
+app.use(cors())
+app.use(route)
+
 app.listen(3001, () => {
     console.log('server running on por 3001')
 })
-
-app.use(route)
-app.use(cors)
